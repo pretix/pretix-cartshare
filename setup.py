9a941fd800fcf3c2197f441605e12e7f93152bbc
Raphael Michel <mail@raphaelmichel.de>
2017-01-04 22:42:06 +0100

    Bump version to 1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+import os
 from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ''
 
 
 setup(
     name='pretix-cartshare',
     version='1.0',
-    description='pretix cartshare',
-    long_description='pretix cartshare',
+    description='pretix plugin to share carts',
+    long_description=long_description,
     url='https://pretix.eu',
     author='Raphael Michel',
     author_email='mail@raphaelmichel.de',
