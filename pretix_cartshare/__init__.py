c16381eae2c1813ff45a2a2050c4e1dc919fd82f
Raphael Michel <mail@raphaelmichel.de>
2017-10-07 20:40:23 +0200

    Bump version

--- pretix_cartshare/__init__.py
+++ pretix_cartshare/__init__.py
@@ -10,7 +10,7 @@ class CartshareApp(AppConfig):
         author = 'Raphael Michel'
         description = 'Allows you to prepare a cart and share it with a customer.'
         visible = True
-        version = '1.1'
+        version = '1.2'
 
     def ready(self):
         from . import signals  # NOQA
