9abd34d7191b6cdc206c2888705bb524e61b2cd2
Raphael Michel <mail@raphaelmichel.de>
2018-06-07 18:22:33 +0200

    Bump release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='pretix-cartshare',
-    version='1.2',
+    version='1.3',
     description='pretix plugin to share carts',
     long_description=long_description,
     url='https://pretix.eu',
